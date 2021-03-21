5862416ae9eb4ee8fec761e1b6817308e198533f
disterzblade <44554790+disterzblade@users.noreply.github.com>
2021-03-21 09:13:04 +0300

    Add files via upload

---
+++ intersections_clusters.py
@@ -0,0 +1,36 @@
+df_new = pd.DataFrame()
+df_new["targets"] = df['targets']
+df_new["preds"] = df['preds']
+
+for i in range(7):
+    globals()['cluster_%s' % i] = 0
+
+for num_clusters in range(7):
+    df_new1 = pd.DataFrame()
+    df_new1 = df_new.loc[df['preds'] == num_clusters]
+    comp = "\n".join(df_new1["targets"].unique())
+    comp = comp.split("\n")
+    
+    for num_clusters_check in range(7):
+        if num_clusters == num_clusters_check:
+            continue
+        else:
+            df_new1 = pd.DataFrame()
+            df_new1 = df_new.loc[df['preds'] == num_clusters_check]
+            comp1 = "\n".join(df_new1["targets"].unique())
+            comp1 = comp1.split("\n")
+            
+            a = comp1
+            b = comp
+            c = []
+            for i in a:
+                for j in b:
+                    if i == j:
+                        c.append(i)
+                        break
+                        
+            globals()['cluster_%s' % num_clusters] = globals()['cluster_%s' % num_clusters] + len(c)
+  
+counts_full = []
+for i in range(7):
+    counts_full.append(globals()['cluster_%s' % i])
